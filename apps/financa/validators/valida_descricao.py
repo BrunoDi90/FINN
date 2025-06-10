d2d7e218faa6b0fbe39f639df8ebd1a11eeacabe
Al <naoimportadescartavel@gmail.com>
2025-06-10 08:58:48 -0300

    FINN ARRUMADO

--- apps/financa/validators/valida_descricao.py
+++ apps/financa/validators/valida_descricao.py
@@ -2,8 +2,8 @@ from django.core.exceptions import ValidationError
 
 
 def valida_descricao(descricao):
-    if len(descricao):
-        raise ValidationError('Descricao tem q ter pelo menos 3 caracteres e não pode ser maior que 300.')
+    if len(descricao) < 3 :
+        raise ValidationError('Descricao tem q ter pelo menos 3 caracteres.')
 
     if isinstance(descricao, int):
         raise ValidationError('Descricao nao pode ser inteiro.')
